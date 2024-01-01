609d5c0c9e4a87c4e8446a537633d8b4d0730073
Daniel Mark Tyson <92043125+danmarktyson@users.noreply.github.com>
2024-01-01 20:42:15 +0100

    Update ts_rbf.R

--- ts_rbf.R
+++ ts_rbf.R
@@ -1,14 +1,14 @@
 ts_rbf <- function(date, alpha, month) {
   if(!is.numeric(month)) stop("'month' argument must be numeric")
   
-  date <- as_date(date)
-  year <- year(date)
+  date <- lubridate::as_date(date)
+  year <- lubridate::year(date)
   
-  start_of_month <- ymd(paste0(year, "-" , month, "-01"))
-  yday_of_month_start <- yday(start_of_month)
-  mid_month <- yday_of_month_start + days_in_month(start_of_month) / 2
+  start_of_month <- lubridate::ymd(paste0(year, "-" , month, "-01"))
+  yday_of_month_start <- lubridate::yday(start_of_month)
+  mid_month <- yday_of_month_start + lubridate::days_in_month(start_of_month) / 2
   
-  day_of_year <- yday(date)
+  day_of_year <- lubridate::yday(date)
   
   return(exp(-1 / (2 * alpha) * (day_of_year - mid_month)^2))
 }
